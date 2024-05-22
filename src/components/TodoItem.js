f317ed7ce301e29bef36767aca7b88ed2bd0e3bd
AthulyaKishor <athulyaambadi1998@gmail.com>
2024-05-22 18:32:39 +0530

    todo list

---
+++ src/components/TodoItem.js
@@ -0,0 +1,17 @@
+import { Card, Checkbox } from '@mui/material';
+import React from 'react'
+import '../styles/styles.css';
+import DeleteIcon from '@mui/icons-material/Delete';
+
+
+const TodoItem=({todo,markComplete,deleteTodo})=> {
+    return (
+        <Card style={{ backgroundColor: "rgb(208 131 131)" }} className='list_container'>
+            <Checkbox  color="success" checked={todo.completed} onChange={() => markComplete(todo.id)} />
+            <h3>{todo.text}</h3>
+            <DeleteIcon className='delete' onClick={()=>{deleteTodo(todo.id)}} />
+        </Card>
+    )
+}
+
+export default TodoItem
